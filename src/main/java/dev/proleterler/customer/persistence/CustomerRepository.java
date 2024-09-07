ad0d746f5441f840ec02de300cf6ce9f724f4ec6
saldiran <kadirsaldiran@gmail.com>
2024-09-07 18:17:30 +0300

    added customer service

--- src/main/java/dev/proleterler/customer/persistence/CustomerRepository.java
+++ src/main/java/dev/proleterler/customer/persistence/CustomerRepository.java
@@ -1,11 +1,13 @@
 package dev.proleterler.customer.persistence;
 
+import dev.proleterler.customer.model.Customer;
 import dev.proleterler.generated.jooq.tables.records.CustomersRecord;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static dev.proleterler.generated.jooq.tables.Customers.CUSTOMERS;
 
@@ -15,8 +17,18 @@ import static dev.proleterler.generated.jooq.tables.Customers.CUSTOMERS;
 public class CustomerRepository {
     private final DSLContext dslContext;
 
+    public Customer save(String name, String email, Optional<String> phoneNumber) {
+        return dslContext.insertInto(CUSTOMERS).columns(CUSTOMERS.NAME, CUSTOMERS.EMAIL, CUSTOMERS.PHONE_NUMBER)
+                .values(name, email, phoneNumber.orElse(""))
+                .returning()
+                .fetchOneInto(Customer.class);
+    }
+
     public List<CustomersRecord> test() {
         return dslContext.selectFrom(CUSTOMERS).limit(10).fetch();
     }
 
+    public int getCustomerCount() {
+        return dslContext.selectCount().from(CUSTOMERS).fetchOneInto(int.class);
+    }
 }
--- src/main/java/dev/proleterler/order/web/OrderController.java
+++ src/main/java/dev/proleterler/order/web/OrderController.java
@@ -10,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
+
+
+
 }
